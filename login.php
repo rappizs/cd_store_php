7be636841a6801274aba0c1558408d2089ca59cb
zsolt <rappizsolt@gmail.com>
2020-11-21 10:36:06 +0100

    Add bootstrap files + design login.php

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bejelentkezés</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <script>
         function blankcheck() {
             const name = document.getElementById("name").value;
@@ -19,31 +20,43 @@
     </script>
 </head>
 
-<body>
-    <h2>Bejelentkezés</h2>
-    <h3 id="error" style="color: red;">
-        <?php
-        if (isset($_GET['nomatch'])) {
-            echo "Hibás felhasználónév vagy jelszó! ";
-        }
-        if (isset($_GET['connectfail'])) {
-            echo "Nem sikerült csatlakozni az adatbázishoz!";
-        }
-        ?>
-    </h3>
-    <h3 style="color: green;">
-        <?php
-        if (isset($_GET['registered'])) {
-            echo "Sikeres regisztráció! :)";
-        }
-        ?>
-    </h3>
-    <form action="check.php" method="post" onsubmit="return blankcheck()">
-        Felhasználónév: <input id="name" name="name" type="text"> <br>
-        Jelszó: <input id="password" name="password" type="password"> <br>
-        <input type="submit" value="Bejelentkezés" name="login">
-    </form>
-    <a href="register.php">Regisztráció</a>
+<body class="bg-light">
+    <div class="container-fluid">
+        <div class="d-flex justify-content-center">
+            <div class="p-5">
+                <h2>Bejelentkezés</h2>
+                <h5 id="error" style="color: red;">
+                    <?php
+                    if (isset($_GET['nomatch'])) {
+                        echo "Hibás felhasználónév vagy jelszó! ";
+                    }
+                    if (isset($_GET['connectfail'])) {
+                        echo "Nem sikerült csatlakozni az adatbázishoz!";
+                    }
+                    ?>
+                </h5>
+                <h3 style="color: green;">
+                    <?php
+                    if (isset($_GET['registered'])) {
+                        echo "Sikeres regisztráció! :)";
+                    }
+                    ?>
+                </h3>
+                <form action="check.php" method="post" onsubmit="return blankcheck()">
+                    <div class="form-group">
+                        <label for="name">Felhasználónév: </label>
+                        <input id="name" name="name" type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Jelszó:</label>
+                        <input id="password" name="password" type="password" class="form-control">
+                    </div>
+                    <input type="submit" value="Bejelentkezés" name="login" class="btn btn-info">
+                    <a href="register.php" class="btn btn-primary">Regisztráció</a>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
